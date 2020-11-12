9f36b8275036eb4bbf97aa637eea8f83f492afa0
Jenn Creighton <jenncreighton@jenns-mbp.home>
2020-11-11 20:41:59 -0500

    feat: yup

---
+++ packages/graphiql/dist/utility/find.d.ts
@@ -0,0 +1,5 @@
+export default function find<T>(
+  list: Array<T>,
+  predicate: (item: T) => boolean,
+): T | void;
+//# sourceMappingURL=find.d.ts.map
---
+++ packages/graphiql/dist/utility/getSelectedOperationName.d.ts
@@ -0,0 +1,7 @@
+import { OperationDefinitionNode } from 'graphql';
+export default function getSelectedOperationName(
+  prevOperations?: OperationDefinitionNode[] | undefined,
+  prevSelectedOperationName?: string,
+  operations?: OperationDefinitionNode[],
+): string | undefined;
+//# sourceMappingURL=getSelectedOperationName.d.ts.map
